e8478f73086dc761cdb96732cee457f1ea5a1d70
gabrielmendoza-nht <ermactodd@gmail.com>
2026-08-27 17:23:38 -0500

    Add files via upload

---
+++ edad.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+    <title>Formulario de entrada del dato</title>
+</head>
+
+<body>
+
+    <form method="post" action="pagina2.php">
+
+        Ingrese su nombre:
+        <input type="text" name="nombre" id="nombre">
+
+        <br>
+        <br>
+
+        Ingrese su edad:
+        <input type="number" name="edad" id="edad">
+
+        <br>
+        <br>
+
+        <input type="submit" value="Confirmar">
+
+    </form>
+
+</body>
+
+</html>
---
+++ pagina2.php
@@ -0,0 +1,15 @@
+<?php
+
+$Nombre = $_REQUEST['nombre'];
+echo "El nombre es: " . $Nombre . "<br>";
+
+$Edad = $_POST["edad"];
+
+if (isset($Edad) and $Edad > 18) {
+    // Acciones
+    echo "Usted puede votar en las próximas elecciones 2028";
+} else {
+    echo "Usted no es mayor de edad";
+}
+
+?>
