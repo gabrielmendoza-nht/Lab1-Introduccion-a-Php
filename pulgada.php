7f03464a7bf01f8ada81c61ae4f2ad053ffaa98d
gabrielmendoza-nht <ermactodd@gmail.com>
2026-08-27 19:55:20 -0500

    Add files via upload

---
+++ pulgada.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="pulg.php" method="post">
+Ingrese las pulgadas para convertir a centimetros:
+    <input type="text" name="numero"><br><br>
+    <input type="submit" value="Convertir">
+</form>
+
+</body>
+</html>
