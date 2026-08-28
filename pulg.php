50937d6788e7e608418bd9b8255800b65fb55cc3
gabrielmendoza-nht <ermactodd@gmail.com>
2026-08-27 19:44:02 -0500

    Add pulg.php file

---
+++ pulg.php
@@ -0,0 +1,15 @@
+<?php
+$numero = $_REQUEST['numero'];
+
+if (isset($_POST['numero'])) {
+    $numero = $_POST['numero'];
+    $centi = $numero * 2.54;
+}   
+
+else{
+    $numero = "No se ha guardado ningun numero";
+}
+
+echo "$numero pulgadas son: $centi centimetros.";
+
+?>
