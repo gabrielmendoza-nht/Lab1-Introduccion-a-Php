c513921ad4a1dd266e993f73d0c1cfb784a880e6
gabrielmendoza-nht <ermactodd@gmail.com>
2026-08-30 08:10:32 -0500

    Enhance pulgada.php with CSS and text updates
    Added a stylesheet link to the HTML head and updated the form prompt text.

--- pulgada.php
+++ pulgada.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="pulgesti.css">
 </head>
 <body>
     
 <form action="pulg.php" method="post">
-Ingrese las pulgadas para convertir a centimetros:
+Ingresa las pulgadas para convertir a cm:
     <input type="text" name="numero"><br><br>
     <input type="submit" value="Convertir">
 </form>
