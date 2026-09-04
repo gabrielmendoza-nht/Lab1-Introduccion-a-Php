793e6e87e32b59b7a7660da980e5c7746d62133e
gabrielmendoza-nht <ermactodd@gmail.com>
2026-09-03 22:54:51 -0500

    Refactor pagina2.php

--- pagina2.php
+++ pagina2.php
@@ -1,15 +1,37 @@
 <?php
 
 $Nombre = $_REQUEST['nombre'];
-echo "El nombre es: " . $Nombre . "<br>";
-
 $Edad = $_POST["edad"];
 
-if (isset($Edad) and $Edad > 18) {
-    // Acciones
-    echo "Usted puede votar en las próximas elecciones 2028";
-} else {
-    echo "Usted no es mayor de edad";
-}
-
 ?>
+
+<html>
+
+<head>
+    <title>Resultado</title>
+    <link rel="stylesheet" href="ed.css">
+</head>
+
+<body>
+
+    <div class="resultado">
+
+        <?php
+
+        echo "<h2>Resultado</h2>";
+
+        echo "Su nombre es: " . $Nombre . "<br><br>";
+
+        if (isset($Edad) and $Edad >= 18) {
+            echo "Usted puede votar en las próximas elecciones 2028";
+        } else {
+            echo "Usted no es mayor de edad";
+        }
+
+        ?>
+
+    </div>
+
+</body>
+
+</html>
