84e39b034338b8f27d5027ae05461b30065870c5
gabrielmendoza-nht <ermactodd@gmail.com>
2026-09-03 22:03:36 -0500

    Delete practica4.c

--- practica4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    float radio, area;
-    printf("inserte el radio del circulo");
-    scanf("%f", &radio);
-    area = 3.1416 * radio * radio;
-    printf("el area del circulo es: %f", area);
-    return 0;
-}
